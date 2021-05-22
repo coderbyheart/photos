a33117bb399b749ea10c25b9d8cc7a248b750fac
Markus Tacker <m@coderbyheart.com>
2021-05-22 22:55:17 +0200

    fix: import only photos

--- import-photo.mjs
+++ import-photo.mjs
@@ -20,32 +20,24 @@ const env = await limiter
 
 const importPhoto = async (photo) => {
   const checksum = (await run('sha256sum', photo)).split(' ')[0].substr(0, 8);
-  let takenAt;
-  let geoInfo;
-  let mediaInfo;
 
-  let contentType = 'image/jpeg';
-  if (/mp4$/i.test(path.parse(photo).ext)) {
-    mediaInfo = JSON.parse(await run('mediainfo', '--Output=JSON', photo));
-    const {
-      media: { track },
-    } = mediaInfo;
-    mediaInfo = track;
-    contentType = 'video/mp4';
-    const [, date, time] = track[0]['Encoded_Date'].split(' ');
-    takenAt = new Date(`${date}T${time}Z`);
-  } else {
-    mediaInfo = await exif(photo);
-    geoInfo = geo(e);
-    takenAt = new Date(
-      e['Exif.Image.DateTime'].replace(
-        /^([0-9]{4}):([0-9]{2}):([0-9]{2}) /,
-        '$1-$2-$3T',
-      ),
-    );
-    if (/png$/i.test(path.parse(photo).ext)) contentType = 'image/png';
-    if (/gif$/i.test(path.parse(photo).ext)) contentType = 'image/gif';
+  const ext = path.parse(photo).ext;
+  if (!['.jpeg', '.jpg'].includes(ext))
+    throw new Error(`Unsupported file extension: ${ext}`);
+  const mediaInfo = await exif(photo);
+  const contentType = 'image/jpeg';
+  const geoInfo = geo(mediaInfo);
+
+  if (mediaInfo === undefined) {
+    throw new Error(`Could read exif data for ${photo}!`);
   }
+
+  const takenAt = new Date(
+    mediaInfo['Exif.Image.DateTime'].replace(
+      /^([0-9]{4}):([0-9]{2}):([0-9]{2}) /,
+      '$1-$2-$3T',
+    ),
+  );
 
   const fileName = `${takenAt
     .toISOString()
